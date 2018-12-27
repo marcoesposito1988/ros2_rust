2b6d223cde89fe5ff501c0f1d08277f56a31188b
Marco Esposito <marco.esposito@tum.de>
2018-12-27 17:12:57 +0100

    Updated for upstream changes in rcl

--- rclrs/src/c/rclrs.c
+++ rclrs/src/c/rclrs.c
@@ -2,17 +2,24 @@
 #include <stdio.h>
 
 #include <rcl/error_handling.h>
+#include <rcl/wait.h>
 #include <rcl/rcl.h>
+#include <rcutils/strdup.h>
 
-bool rclrs_native_ok() { return rcl_ok(); }
+static rcl_context_t default_context;
+
 
 int32_t rclrs_native_init() {
   // TODO(esteve): parse args
-  return rcl_init(0, NULL, rcl_get_default_allocator());
+  rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+  rcl_ret_t ret = rcl_init_options_init(&init_options, rcutils_get_default_allocator());
+  if (ret != 0)
+      return ret;
+  return rcl_init(0, NULL, &init_options, &default_context);
 }
 
 const char *rclrs_native_get_error_string_safe() {
-  return rcl_get_error_string_safe();
+  return rcutils_strdup(rcl_get_error_string().str, rcutils_get_default_allocator());
 }
 
 void rclrs_native_reset_error() { rcl_reset_error(); }
@@ -24,7 +31,7 @@ int32_t rclrs_native_create_node_handle(uintptr_t *node_handle,
   *node = rcl_get_zero_initialized_node();
 
   rcl_node_options_t default_options = rcl_node_get_default_options();
-  rcl_ret_t ret = rcl_node_init(node, name, namespace, &default_options);
+  rcl_ret_t ret = rcl_node_init(node, name, namespace, &default_context, &default_options);
   *node_handle = (uintptr_t)node;
   return ret;
 }
@@ -93,23 +100,9 @@ int32_t rclrs_native_wait_set_init(uintptr_t wait_set_handle,
   return ret;
 }
 
-int32_t rclrs_native_wait_set_clear_subscriptions(uintptr_t wait_set_handle) {
+int32_t rclrs_native_wait_set_clear(uintptr_t wait_set_handle) {
   rcl_wait_set_t *wait_set = (rcl_wait_set_t *)wait_set_handle;
-  rcl_ret_t ret = rcl_wait_set_clear_subscriptions(wait_set);
-
-  return ret;
-}
-
-int32_t rclrs_native_wait_set_clear_services(uintptr_t wait_set_handle) {
-  rcl_wait_set_t *wait_set = (rcl_wait_set_t *)wait_set_handle;
-  rcl_ret_t ret = rcl_wait_set_clear_services(wait_set);
-
-  return ret;
-}
-
-int32_t rclrs_native_wait_set_clear_clients(uintptr_t wait_set_handle) {
-  rcl_wait_set_t *wait_set = (rcl_wait_set_t *)wait_set_handle;
-  rcl_ret_t ret = rcl_wait_set_clear_clients(wait_set);
+  rcl_ret_t ret = rcl_wait_set_clear(wait_set);
 
   return ret;
 }
@@ -118,7 +111,7 @@ int32_t rclrs_native_wait_set_add_subscription(uintptr_t wait_set_handle,
                                                uintptr_t subscription_handle) {
   rcl_wait_set_t *wait_set = (rcl_wait_set_t *)wait_set_handle;
   rcl_subscription_t *subscription = (rcl_subscription_t *)subscription_handle;
-  rcl_ret_t ret = rcl_wait_set_add_subscription(wait_set, subscription);
+  rcl_ret_t ret = rcl_wait_set_add_subscription(wait_set, subscription, NULL); // TODO: use index returned as out param?
 
   return ret;
 }
